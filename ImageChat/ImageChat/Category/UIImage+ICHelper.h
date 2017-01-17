358e97c1a42e397810561fc27fe1cff4b116d3bf
cr7bean <cr7bean@gmail.com>
2017-01-17 19:03:31 +0800

    添加拍照模块

---
+++ ImageChat/ImageChat/Category/UIImage+ICHelper.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+ICHelper.h
+//  ImageChat
+//
+//  Created by FigureWang on 17/1/17.
+//  Copyright © 2017年 Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ICHelper)
+
+@end
---
+++ ImageChat/ImageChat/Category/UIImage+ICHelper.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+ICHelper.m
+//  ImageChat
+//
+//  Created by FigureWang on 17/1/17.
+//  Copyright © 2017年 Long. All rights reserved.
+//
+
+#import "UIImage+ICHelper.h"
+
+@implementation UIImage (ICHelper)
+
+@end
